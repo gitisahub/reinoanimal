34b36cfb22acea1f165b1d54844e653d05e84bd4
María Navarro Elbal <maria.navarro4@murciaeduca.es>
2026-03-01 18:13:08 +0100

    Aplicada refactorización al código: ocultación de atributos, constructores con nombre y método toString

--- src/main/java/paqAnimales/Gato.java
+++ src/main/java/paqAnimales/Gato.java
@@ -1,13 +1,30 @@
 package paqAnimales;
 
+/**
+ * Representa un gato dentro del reino animal.
+ * Es un mamífero que puede maullar y tiene un número de pelos.
+ */
 public class Gato extends Mamifero {
+    private int pelos;
+    public Gato(String nombre) {
+        super(nombre);
+    }
 
-    public int pelos;
+    public int getPelos() {
+        return pelos;
+    }
+
+    public void setPelos(int pelos) {
+        this.pelos = pelos;
+    }
 
     @Override
     void dormir() {
         System.out.println("El gato se pasa todo el día durmiendo");
     }
+    /**
+     * Permite que el gato emita el sonido de maullar.
+     */
 
     void maullar() {
         System.out.println("Es lo que hacen los gatos");
@@ -15,7 +32,7 @@ public class Gato extends Mamifero {
 
     @Override
     void relacionar(Animal p) {
-        System.out.println("Con el gato: " + p.nombre);
+        System.out.println("Con el gato: " + p.toString());
     }
 
 }
--- src/main/java/paqAnimales/Mamifero.java
+++ src/main/java/paqAnimales/Mamifero.java
@@ -1,7 +1,13 @@
 package paqAnimales;
-
+/**
+ * Representa un mamífero dentro del reino animal.
+ * Hereda de la clase Animal y define comportamientos específicos.
+ */
 public class Mamifero extends Animal {
 
+    public Mamifero(String nombre) {
+        super(nombre);
+    }
     @Override
     void reproducir() {
         System.out.println("La reproduccion es vivipara");
@@ -9,6 +15,6 @@ public class Mamifero extends Animal {
 
     @Override
     void relacionar(Animal m) {
-        System.out.println("Con el mamifero: " + m.nombre);
+        System.out.println("Con el mamifero: " + m.toString());
     }
 }
